b5a164081f35ec4868f4ca6ff6919f600f0bfa06
lazzy <arthurrgg8@gmail.com>
2026-05-26 21:40:41 +0545

    Refactor: Correct npmrc legacy-peer-deps syntax, configure ESLint 9 global ignores, and add secure HTTP headers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+const config = [
+  {
+    ignores: [
+      ".next/**",
+      ".vercel/**",
+      ".wrangler/**",
+      "node_modules/**",
+      "next-env.d.ts",
+    ],
+  },
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    ".vercel/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+];
 
-export default eslintConfig;
+export default config;
